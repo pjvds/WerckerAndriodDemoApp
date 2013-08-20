52eba427c9500228ebd7785a17e984b1f9aab842
Pieter Joost van de Sande <pj@born2code.net>
2013-08-20 11:24:45 +0200

    Adds build folder to .gitignore

--- WerckerAndriodDemoApp/build/source/buildConfig/debug/com/wercker/WerckerAndriodDemoApp/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.wercker.WerckerAndriodDemoApp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
