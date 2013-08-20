b6eaec3c8657330ffb08cf9814f066c18513446c
Pieter Joost van de Sande <pj@born2code.net>
2013-08-20 10:53:07 +0200

    Adds demo sample project
    This project was created by the wizard from Andriod Studio.

---
+++ WerckerAndriodDemoApp/build/source/buildConfig/debug/com/wercker/WerckerAndriodDemoApp/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wercker.WerckerAndriodDemoApp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+
+}
